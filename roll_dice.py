bd55614ffa6f9d8331c3f4fc0d9b39e4f9bcdc3a
TheKallman <zac@kallmans.com>
2024-01-26 14:49:32 -0600

    Update roll_dice.py

--- roll_dice.py
+++ roll_dice.py
@@ -3,4 +3,8 @@ import random
 def roll_die():
     #accepts no arguments
     #returns a random integer from 1 to 6
+    for x in range(12):
+        dice = random.randint(1, 6)
+        print(dice)
+    
     return dice
