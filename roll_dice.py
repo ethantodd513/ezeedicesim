7a8f8ccb23e57671193885ec567b393c962186f6
TheKallman <zac@kallmans.com>
2024-02-01 14:20:40 -0600

    Update roll_dice.py

--- roll_dice.py
+++ roll_dice.py
@@ -3,8 +3,12 @@ import random
 def roll_die():
     #accepts no arguments
     #returns a random integer from 1 to 6
+<<<<<<< Updated upstream
     for x in range(12):
         dice = random.randint(1, 6)
         print(dice)
     
+=======
+    dice = random.randint(1, 6)
+>>>>>>> Stashed changes
     return dice
