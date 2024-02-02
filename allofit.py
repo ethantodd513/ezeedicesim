74eb79324a747620be306ff115577b11ec2bf144
TheKallman <zac@kallmans.com>
2024-02-02 14:51:39 -0600

    Update allofit.py

--- allofit.py
+++ allofit.py
@@ -1,11 +1,10 @@
 import random
 
-def roll_die():
-    #accepts no arguments
-    #returns a random integer from 1 to 6
-
-    return random.randint(1, 6)
-
+def main():
+    #no arguments
+    #call all functions to play the number of games specified
+    first_roll()
+    
 def first_roll():
     #no arguments
     #uses roll_die() to generate list of 12 integers
@@ -18,6 +17,24 @@ def first_roll():
         dice.append(dieroll)
         
     return(dice)
+    
+def roll_die():
+    #accepts no arguments
+    #returns a random integer from 1 to 6
+
+    return random.randint(1, 6)
 
 def output_dice(dice):
+    #accepts dice
+    #outputs each die in the list
+    
+    print(dice)
+    
+def poop():
     pass
+def apples():
+    poop = 19
+    for poooo in range(12):
+        poop += 1
+        
+    print(poop)
